5fe1652df3cab71c5c8298f87deb334e0f047440
satvik8373 <satvikpatel8373@gmail.com>
2025-03-13 18:03:46 +0530

    asss

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -95,7 +95,6 @@ function App() {
               <Route path="/login" element={<Login />} />
               <Route path="/forgot-password" element={<ForgotPassword />} />
               <Route path="/reset-password/:token" element={<ResetPassword />} />
-              <Route path="/reset-password/*" element={<ResetPassword />} />
               <Route path="/admin/*" element={
                 <ProtectedRoute requireAdmin={true}>
                   <Admin />
